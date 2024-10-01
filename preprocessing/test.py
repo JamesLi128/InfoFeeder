d4448ad8efba26aceee7b16f4dea9af39a25af1c
JamesLi128 <jamesli.wks@gmail.com>
2024-10-01 15:20:52 -0400

    parallel graph scrap realized

---
+++ preprocessing/test.py
@@ -0,0 +1,8 @@
+from utils import Collaboration_Graph_Scraper
+
+anchor_author = "Amitabh Basu"
+category_ls = ['cs.LG', 'cs.AI', 'math.CO', 'stat.ML']
+scraper = Collaboration_Graph_Scraper(anchor_author=anchor_author, category_ls=category_ls)
+
+max_num_workers = 5
+scraper.build_collaboration_graph_from_author(max_workers=max_num_workers)
--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -11,17 +11,11 @@ import concurrent.futures
 import threading
 import tiktoken
 import fitz
-
+import queue
 from openai import OpenAI
 import torch
-
+import time
 class Collaboration_Graph_Scraper:
-    """
-    Firstly read from cache graph if exists, otherwise start from scratch.
-    Need to offer an anchor point, like a paper or an author or a category.
-    max_depth, max_results per etc. can be configured
-    if multiple max values are set, the lowest will be used.
-    """
     def __init__(self, cache_path : str = None, 
                  save_path : str = '../data/collaboration_graph.pkl',
                  anchor_author : str = None, 
@@ -41,154 +35,271 @@ class Collaboration_Graph_Scraper:
         # self.max_collaborations = max_collaborations
         # self.max_authors = max_authors
         # self.max_papers = max_papers
+        self.authors_to_explore = queue.Queue()
+        self.depth_queue = queue.Queue()
+
         self._load_graph()
         if anchor_author not in self.graph:
             self.graph.add_node(anchor_author)
+            self.authors_to_explore.put(anchor_author)
+            self.depth_queue.put(0)
+        
+        self.processed_authors = self.fully_explored_authors.copy()
 
     def _load_graph(self) -> None:
-        """
-        Load the collaboration graph from the cache if it exists.
-        """
         if self.cache_path != None and os.path.exists(self.cache_path):
             with open(self.cache_path, 'rb') as f:
-                self.graph, self.paper_id2author_ls = pickle.load(f)
+                self.graph, self.paper_id2author_ls, self.author2paper_id, self.fully_explored_authors = pickle.load(f)
         else:
             self.graph = nx.Graph()
             self.paper_id2author_ls = {}
+            self.author2paper_id = {}
+            self.fully_explored_authors = set()
 
     def _author_re(self, author_name : str) -> re.Pattern:
-        """
-        Generate a regex pattern for the given author name.
-        
-        Args:
-            author_name (str): The name of the author.
-            
-        Returns:
-            re.Pattern: A compiled regex pattern.
-        """
         split = author_name.split(' ')
         middle_str = r'\s*'.join(split)
         final_str = f"^{middle_str}$"
         return re.compile(final_str, re.IGNORECASE)
-        
-    def search_step(self, unexplored_authors : list, depth : int = 0, func_id=0) -> None:
-        print(f"current depth: {depth}, unexplored authors: {unexplored_authors}")
-        """
-        Perform a search step to find collaborators and papers.
-        
-        Args:
-            depth (int): The current depth of the search.
-        """
-        # Implement the logic for searching collaborators and papers
+    
+    def author_search_step(self, author_name : str, lock : threading.Lock, depth : int) -> None:
+
+        time.sleep(1)
 
         if depth > self.max_depth:
             return
-
-        else:
-            new_unexplored_authors = []
-            for author in tqdm(unexplored_authors, desc=f"Depth {depth}, Func {func_id}"):
-                author_re = self._author_re(author_name=author)
-                search_url = url_generator(author_ls=[author], max_results=self.max_results, category_ls=self.category_ls)
-                feed = feedparser.parse(search_url)
-                for entry in feed.entries:
-                    for searched_author in entry.authors:
-                        # print(searched_author.name)
-                        if author_re.match(searched_author.name) is not None:
-                            paper_id = entry.id.split('/abs/')[-1]
-                            if paper_id not in self.paper_id2author_ls:
-                                author_name_ls = [au_dict.name for au_dict in entry.authors if au_dict.name not in self.graph]
-                                self.paper_id2author_ls[paper_id] = author_name_ls
-                                new_unexplored_authors.extend(author_name_ls)
-                                for co_author in entry.authors:
-                                    if co_author.name not in self.graph:
-                                        self.graph.add_node(co_author.name)
-                                    if self.graph.has_edge(author, co_author.name):
-                                        self.graph[author][co_author.name]['weight'] += 1
-                                    else:
-                                        self.graph.add_edge(author, co_author.name)
-                                        self.graph[author][co_author.name]['weight'] = 1
-            
-            self.search_step(depth=depth+1, unexplored_authors=new_unexplored_authors, func_id=func_id+1)
-
-
-    def parallel_search_step(self, unexplored_authors: list, depth: int = 0, func_id : int=0) -> None:
-        print(f"Starting search at depth: {depth}, number of authors: {len(unexplored_authors)}")
-
-        if depth > self.max_depth:
-            return
-
-        new_unexplored_authors = []
-        author_lock = threading.Lock()
-        graph_lock = threading.Lock()
-        paper_record_lock = threading.Lock()
-
-        def process_author(author):
-            nonlocal new_unexplored_authors
-            author_re = self._author_re(author_name=author)
-            search_url = url_generator(author_ls=[author], max_results=self.max_results, category_ls=self.category_ls)
-            feed = feedparser.parse(search_url)
-            local_new_authors = []
-
-            for entry in feed.entries:
+        arxiv_query_url = url_generator(author_ls=[author_name], max_results=self.max_results, category_ls=self.category_ls)
+        feed = feedparser.parse(arxiv_query_url)
+        new_authors = []
+        crnt_author_re = self._author_re(author_name)
+        with lock:
+            for entry in tqdm(feed.entries, desc=f"Author {author_name} at Depth {depth}"):
+                paper_id = entry.id.split('/abs/')[-1]
                 for searched_author in entry.authors:
-                    if author_re.match(searched_author.name) is not None:
-                        paper_id = entry.id.split('/abs/')[-1]
-                        
-                        with paper_record_lock:
-                            if paper_id in self.paper_id2author_ls:
-                                continue
-                            author_name_ls = [au_dict.name for au_dict in entry.authors if au_dict.name not in self.graph]
-                            self.paper_id2author_ls[paper_id] = author_name_ls
+                    if crnt_author_re.match(searched_author.name) and paper_id not in self.paper_id2author_ls:
+                        collaborator_name_ls = [collab_dict.name for collab_dict in entry.authors if collab_dict.name not in self.processed_authors]
+                        self.paper_id2author_ls[paper_id] = collaborator_name_ls
+                        for collaborator_name in collaborator_name_ls:
+                            self.authors_to_explore.put(collaborator_name)
+                            self.depth_queue.put(depth+1)
+                            if collaborator_name not in self.graph:
+                                self.graph.add_node(collaborator_name)
+                            if self.graph.has_edge(author_name, collaborator_name):
+                                self.graph[author_name][collaborator_name]['weight'] += 1
+                            else:
+                                self.graph.add_edge(author_name, collaborator_name)
+                                self.graph[author_name][collaborator_name]['weight'] = 1
+                            if collaborator_name not in self.author2paper_id:
+                                self.author2paper_id[collaborator_name] = set()
+                            self.author2paper_id[collaborator_name].add(paper_id)
+        self.fully_explored_authors.add(author_name)
 
-                        local_new_authors.extend(author_name_ls)
+    def build_collaboration_graph_from_author(self, max_workers : int = 8) -> None:
+        start_time = time.time()
+        lock = threading.Lock()
 
-                        with graph_lock:
-                            for co_author in entry.authors:
-                                co_name = co_author.name
-                                if co_name not in self.graph:
-                                    self.graph.add_node(co_name)
-                                if self.graph.has_edge(author, co_name):
-                                    self.graph[author][co_name]['weight'] += 1
-                                else:
-                                    self.graph.add_edge(author, co_name)
-                                    self.graph[author][co_name]['weight'] = 1
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            futures = []
 
-            with author_lock:
-                new_unexplored_authors.extend(local_new_authors)
-
-        # Use ThreadPoolExecutor to process authors in parallel
-        max_workers = min(8, len(unexplored_authors) or 1)  # Adjust the number of workers as needed
-        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-            # Using tqdm to show progress
-            futures = {executor.submit(process_author, author): author for author in unexplored_authors}
-            for future in tqdm(concurrent.futures.as_completed(futures), total=len(futures),
-                            desc=f"Depth {depth}, Func {func_id}"):
+            while (not self.authors_to_explore.empty()) or any([future.running() for future in futures]):
                 try:
-                    future.result()
-                except Exception as exc:
-                    author = futures[future]
-                    print(f'Author {author} generated an exception: {exc}')
+                    crnt_author = self.authors_to_explore.get(timeout=2)
+                    crnt_depth = self.depth_queue.get()
+                    self.processed_authors.add(crnt_author)
+                    future = executor.submit(self.author_search_step, crnt_author, lock, crnt_depth)
+                    futures.append(future)
 
-        # Remove duplicates by converting to a set
-        unique_new_authors = list(set(new_unexplored_authors))
+                except queue.Empty:
+                    pass
 
-        # Recursive call for the next depth level
-        self.search_step(depth=depth + 1, unexplored_authors=unique_new_authors, func_id=func_id + 1)
-
-
+            for future in futures:
+                future.result()
+        self.save_graph()
+        end_time = time.time()
+        print(f"Graph built in {end_time - start_time:.2f} seconds.")
 
     def save_graph(self, save_path : str = None) -> None:
-        """
-        Save the collaboration graph to the given path.
-        
-        Args:
-            save_path (str): The path to save the graph.
-        """
         if save_path is None:
             save_path = self.save_path
         with open(save_path, 'wb') as f:
-            pickle.dump((self.graph, self.paper_id2author_ls), f)
+            pickle.dump((self.graph, self.paper_id2author_ls, self.author2paper_id, self.fully_explored_authors), f)
         print(f"Graph saved to {save_path}")
+
+
+# class Collaboration_Graph_Scraper:
+#     """
+#     Firstly read from cache graph if exists, otherwise start from scratch.
+#     Need to offer an anchor point, like a paper or an author or a category.
+#     max_depth, max_results per etc. can be configured
+#     if multiple max values are set, the lowest will be used.
+#     """
+#     def __init__(self, cache_path : str = None, 
+#                  save_path : str = '../data/collaboration_graph.pkl',
+#                  anchor_author : str = None, 
+#                  anchor_paper : str = None, 
+#                  anchor_category : str = None, 
+#                  category_ls : list = None,
+#                  max_depth : int = 3,
+#                  max_results : int = 200) -> None:
+#         self.cache_path = cache_path
+#         self.save_path = save_path
+#         self.anchor_author = anchor_author
+#         self.anchor_paper = anchor_paper
+#         self.anchor_category = anchor_category
+#         self.category_ls = category_ls
+#         self.max_depth = max_depth
+#         self.max_results = max_results
+#         # self.max_collaborations = max_collaborations
+#         # self.max_authors = max_authors
+#         # self.max_papers = max_papers
+#         self._load_graph()
+#         if anchor_author not in self.graph:
+#             self.graph.add_node(anchor_author)
+
+#     def _load_graph(self) -> None:
+#         """
+#         Load the collaboration graph from the cache if it exists.
+#         """
+#         if self.cache_path != None and os.path.exists(self.cache_path):
+#             with open(self.cache_path, 'rb') as f:
+#                 self.graph, self.paper_id2author_ls = pickle.load(f)
+#         else:
+#             self.graph = nx.Graph()
+#             self.paper_id2author_ls = {}
+
+#     def _author_re(self, author_name : str) -> re.Pattern:
+#         """
+#         Generate a regex pattern for the given author name.
+        
+#         Args:
+#             author_name (str): The name of the author.
+            
+#         Returns:
+#             re.Pattern: A compiled regex pattern.
+#         """
+#         split = author_name.split(' ')
+#         middle_str = r'\s*'.join(split)
+#         final_str = f"^{middle_str}$"
+#         return re.compile(final_str, re.IGNORECASE)
+        
+#     def search_step(self, unexplored_authors : list, depth : int = 0, func_id=0) -> None:
+#         print(f"current depth: {depth}, unexplored authors: {unexplored_authors}")
+#         """
+#         Perform a search step to find collaborators and papers.
+        
+#         Args:
+#             depth (int): The current depth of the search.
+#         """
+#         # Implement the logic for searching collaborators and papers
+
+#         if depth > self.max_depth:
+#             return
+
+#         else:
+#             new_unexplored_authors = []
+#             for author in tqdm(unexplored_authors, desc=f"Depth {depth}, Func {func_id}"):
+#                 author_re = self._author_re(author_name=author)
+#                 search_url = url_generator(author_ls=[author], max_results=self.max_results, category_ls=self.category_ls)
+#                 feed = feedparser.parse(search_url)
+#                 for entry in feed.entries:
+#                     for searched_author in entry.authors:
+#                         # print(searched_author.name)
+#                         if author_re.match(searched_author.name) is not None:
+#                             paper_id = entry.id.split('/abs/')[-1]
+#                             if paper_id not in self.paper_id2author_ls:
+#                                 author_name_ls = [au_dict.name for au_dict in entry.authors if au_dict.name not in self.graph]
+#                                 self.paper_id2author_ls[paper_id] = author_name_ls
+#                                 new_unexplored_authors.extend(author_name_ls)
+#                                 for co_author in entry.authors:
+#                                     if co_author.name not in self.graph:
+#                                         self.graph.add_node(co_author.name)
+#                                     if self.graph.has_edge(author, co_author.name):
+#                                         self.graph[author][co_author.name]['weight'] += 1
+#                                     else:
+#                                         self.graph.add_edge(author, co_author.name)
+#                                         self.graph[author][co_author.name]['weight'] = 1
+            
+#             self.search_step(depth=depth+1, unexplored_authors=new_unexplored_authors, func_id=func_id+1)
+
+
+#     def parallel_search_step(self, unexplored_authors: list, depth: int = 0, func_id : int=0) -> None:
+#         print(f"Starting search at depth: {depth}, number of authors: {len(unexplored_authors)}")
+
+#         if depth > self.max_depth:
+#             return
+
+#         new_unexplored_authors = []
+#         author_lock = threading.Lock()
+#         graph_lock = threading.Lock()
+#         paper_record_lock = threading.Lock()
+
+#         def process_author(author):
+#             nonlocal new_unexplored_authors
+#             author_re = self._author_re(author_name=author)
+#             search_url = url_generator(author_ls=[author], max_results=self.max_results, category_ls=self.category_ls)
+#             feed = feedparser.parse(search_url)
+#             local_new_authors = []
+
+#             for entry in feed.entries:
+#                 for searched_author in entry.authors:
+#                     if author_re.match(searched_author.name) is not None:
+#                         paper_id = entry.id.split('/abs/')[-1]
+                        
+#                         with paper_record_lock:
+#                             if paper_id in self.paper_id2author_ls:
+#                                 continue
+#                             author_name_ls = [au_dict.name for au_dict in entry.authors if au_dict.name not in self.graph]
+#                             self.paper_id2author_ls[paper_id] = author_name_ls
+
+#                         local_new_authors.extend(author_name_ls)
+
+#                         with graph_lock:
+#                             for co_author in entry.authors:
+#                                 co_name = co_author.name
+#                                 if co_name not in self.graph:
+#                                     self.graph.add_node(co_name)
+#                                 if self.graph.has_edge(author, co_name):
+#                                     self.graph[author][co_name]['weight'] += 1
+#                                 else:
+#                                     self.graph.add_edge(author, co_name)
+#                                     self.graph[author][co_name]['weight'] = 1
+
+#             with author_lock:
+#                 new_unexplored_authors.extend(local_new_authors)
+
+#         # Use ThreadPoolExecutor to process authors in parallel
+#         max_workers = min(8, len(unexplored_authors) or 1)  # Adjust the number of workers as needed
+#         with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+#             # Using tqdm to show progress
+#             futures = {executor.submit(process_author, author): author for author in unexplored_authors}
+#             for future in tqdm(concurrent.futures.as_completed(futures), total=len(futures),
+#                             desc=f"Depth {depth}, Func {func_id}"):
+#                 try:
+#                     future.result()
+#                 except Exception as exc:
+#                     author = futures[future]
+#                     print(f'Author {author} generated an exception: {exc}')
+
+#         # Remove duplicates by converting to a set
+#         unique_new_authors = list(set(new_unexplored_authors))
+
+#         # Recursive call for the next depth level
+#         self.search_step(depth=depth + 1, unexplored_authors=unique_new_authors, func_id=func_id + 1)
+
+
+
+#     def save_graph(self, save_path : str = None) -> None:
+#         """
+#         Save the collaboration graph to the given path.
+        
+#         Args:
+#             save_path (str): The path to save the graph.
+#         """
+#         if save_path is None:
+#             save_path = self.save_path
+#         with open(save_path, 'wb') as f:
+#             pickle.dump((self.graph, self.paper_id2author_ls), f)
+#         print(f"Graph saved to {save_path}")
 
 
 class EmbeddingGenerator:
