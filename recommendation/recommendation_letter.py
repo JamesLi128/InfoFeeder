647926b7d30988bffd9fa3fe48e6f6c0c64d9bfb
JamesLi128 <jamesli.wks@gmail.com>
2024-10-28 15:14:24 -0400

    prototype recommendation finished

---
+++ recommendation/recommendation_letter.py
@@ -0,0 +1,10 @@
+import pickle
+import os
+from utils import RecommendationLetterSeeker
+
+affiliation_path = '../data/author_affiliation/author_affiliation_parallel.pkl'
+collaboration_graph_path = '../data/collaboration_graph.pkl'
+scholar_name = 'Amitabh Basu'
+letter_seeker = RecommendationLetterSeeker(author_affiliation_path=affiliation_path, collaboration_graph_path=collaboration_graph_path)
+_, collaborators, affiliations = letter_seeker.from_scholar_name2affiliation(scholar_name=scholar_name)
+letter_seeker.beautiful_print(scholar_name=scholar_name, collaborators=collaborators, affiliations=affiliations)
--- recommendation/utils.py
+++ recommendation/utils.py
@@ -4,6 +4,8 @@ import torch.nn.functional as F
 import numpy as np
 import time
 import os
+import pickle
+import networkx as nx
 
 from tqdm import tqdm
 
@@ -73,6 +75,59 @@ class User:
     def get_paper_set(self):
         return self.Paper_set
 
+class RecommendationLetterSeeker: 
+    '''
+    Based on affiliation dictionary, collaboration graph, and given info, recommend scholars to write recommendation letters.
+    '''
+    def __init__(self, author_affiliation_path : str, collaboration_graph_path : str):
+        _, self.affiliation_dict = self._load_affiliation_dict(author_affiliation_path)
+        self.collaboration_graph, self.paper_id2author_ls = self._load_collaboration_graph(collaboration_graph_path)
+
+    def _load_affiliation_dict(self, path : str):
+        with open(path, 'rb') as f:
+            affiliation_dict = pickle.load(f)
+        return affiliation_dict
+    
+    def _load_collaboration_graph(self, path : str):
+        with open(path, 'rb') as f:
+            collaboration_graph, paper_id2author_ls, _, _ = pickle.load(f)
+        return collaboration_graph, paper_id2author_ls
+
+    def from_scholar_name2affiliation(self, scholar_name : str):
+        if scholar_name not in self.collaboration_graph.nodes:
+            print(f"Query: scholar '{scholar_name}' not found in collaboration graph.")
+            return None
+
+        collaborators = list(self.collaboration_graph[scholar_name].keys())
+        affiliations = []
+        for collaborator in collaborators:
+            collaborator = collaborator.lower()
+            if collaborator in self.affiliation_dict:
+                rst = self.affiliation_dict[collaborator]
+                institution_ls = list(rst.keys())
+                departments_ls = list(rst.values())
+            else:
+                institution_ls = []
+                departments_ls = []
+            affiliations.append((institution_ls, departments_ls))
+        return (scholar_name, collaborators, affiliations)
+    
+    def beautiful_print(self, scholar_name : str, collaborators : list[str], affiliations : list[tuple[str, list[str]]]):
+        print(f"Scholar: {scholar_name}")
+        print("Collaborators and their affiliations:")
+        for collaborator, (institution_ls, departments_ls) in zip(collaborators, affiliations):
+            institution_ls = [i for i in institution_ls if i != "N/A"]  # Filter out empty institutions
+            print(f" - {collaborator}: ")
+            if len(institution_ls) == 0 and len(departments_ls) == 0:
+                print(f"\tNo affiliation found.")
+                continue
+            for institution, departments in zip(institution_ls, departments_ls):
+                departments = [d for d in departments if d != "N/A"]  # Filter out empty departments
+                print(f"\t{institution}, \tDepartments: {', '.join(departments)}")
+
 
 def paper_id2paper_name(paper_id : str) -> str:
     return paper_id.replace('/', '_')
+
+def paper_name2paper_id(paper_name : str) -> str:
+    return paper_name.replace('_', '/')
